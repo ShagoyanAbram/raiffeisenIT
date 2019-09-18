7dbaa7a4096f94db99a2ef210b712078152d8fd1
dtsyganov-trainings <55183964+dtsyganov-trainings@users.noreply.github.com>
2019-09-18 19:40:57 +0600

    Update HotCold.java

--- src/main/java/com/abram/game/HotCold.java
+++ src/main/java/com/abram/game/HotCold.java
@@ -2,6 +2,7 @@ package com.abram.game;
 
 import java.util.Scanner;
 
+//зачем отдельный класс, если все его методы статические ? 
 public class HotCold {
 
     public static int randomNumber(final int max) {
@@ -13,6 +14,7 @@ public class HotCold {
             Scanner sc = new Scanner(System.in);
             int player = sc.nextInt();
 
+            //условия неверны
             if (player == random) {
                 System.out.println("Успех");
                 break;
