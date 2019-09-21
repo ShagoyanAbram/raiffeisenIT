4f020ec136142c99ef18d917e3cc3d01ef404b88
abram <abramshagoyan@gmail.com>
2019-09-21 13:39:17 +0600

    HW_TWO

---
+++ src/main/java/com/abram/leak/MemoryLeak.java
@@ -0,0 +1,17 @@
+package com.abram.leak;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryLeak {
+        public static List<Long> list = new ArrayList<>();
+
+        public static void main(String[] args) {
+            long i = 0;
+            while (true){
+                list.add(i);
+                i++;
+            }
+        }
+
+}
