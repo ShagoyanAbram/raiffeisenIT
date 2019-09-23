2757197a6e3f9157620507fa3591450de9523b51
abram <abramshagoyan@gmail.com>
2019-09-23 21:55:57 +0600

    I.Java core homework six object, memory leak

---
+++ src/main/java/com/abram/homework/six/leak/MemoryLeak.java
@@ -0,0 +1,16 @@
+package com.abram.homework.six.leak;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryLeak {
+    public static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        long i = 0;
+        while (true) {
+            list.add(i);
+            i++;
+        }
+    }
+}
